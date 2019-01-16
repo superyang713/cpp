7a2e659897004315261502e7b3dc0ddda2315ab0
Yang Dai <yang@Yangs-MacBook-Pro.local>
2019-01-16 00:19:45 -0500

    working with classes, and split them into header and source files.

---
+++ b1/08_classes/01-simple-class-splitted-into-different-files/Pen.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Pen.h"
+
+void Pen::write_on_paper(std::string words) {
+  if (InkLevelPrecent <= 0) {
+    std::cout << "Oops, Out of ink!" << "\n";
+  }
+  else {
+    std::cout << words << "\n";
+    InkLevelPrecent = InkLevelPrecent = words.length();
+  }
+}
+
+void Pen::break_in_half() {
+  InkLevelPrecent = InkLevelPrecent / 2;
+  Length = Length / 2.0;
+}
+
+void Pen::run_out_of_ink() {
+  InkLevelPrecent = 0;
+}
---
+++ b1/08_classes/01-simple-class-splitted-into-different-files/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Pen.h"
+
+int main()
+{
+  Pen FavoritePen;
+
+  FavoritePen.InkColor = blue;
+  FavoritePen.ShellColor = grey;
+  FavoritePen.CapColor = blue;
+  FavoritePen.Style = ballpoint;
+  FavoritePen.Length = 5.5;
+  FavoritePen.Brand = "office Depot";
+  FavoritePen.InkLevelPrecent = 30;
+
+  Pen WorstPen;
+
+  WorstPen.InkColor = red;
+  WorstPen.ShellColor = grey;
+  WorstPen.CapColor = black;
+  WorstPen.Style = fountain_pen;
+  WorstPen.Length = 5.0;
+  WorstPen.Brand = "Parker";
+  WorstPen.InkLevelPrecent = 60;
+
+  std::cout << "This is my favorite pen" << "\n";
+  std::cout << "Color: " << FavoritePen.InkColor << "\n";
+  std::cout << "Brand" << FavoritePen.Brand << "\n";
+  std::cout << "Ink Level: " << FavoritePen.InkLevelPrecent << "%" << "\n";
+
+  FavoritePen.write_on_paper("Hello I am a pen");
+  std::cout << "Ink Level: " << FavoritePen.InkLevelPrecent << "%" << "\n";
+        
+  return 0;
+}
